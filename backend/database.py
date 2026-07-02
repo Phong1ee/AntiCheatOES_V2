a68208cd3e72c1d39329cf5680926d684de657c2
hatorann <haotran1311@gmail.com>
2026-07-02 10:15:09 +0700

    orm applied

---
+++ backend/database.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv(Path(__file__).resolve().parent / ".env")
+
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_NAME = os.getenv("DB_NAME", "")
+
+URL_DATABASE = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine = create_engine(URL_DATABASE, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI, HTTPException, Depends, status
+from pydantic import BaseModel
+from typing import Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main__":
-    main()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
